843d362b1a76acfa1c95ad2dd8d286447626aa68
Alethepata <alessia.patane@gmail.com>
2024-01-13 17:06:45 +0100

    [edit] fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'surname',
+        'date_of_birth',
+        'password'
     ];
 
     /**
