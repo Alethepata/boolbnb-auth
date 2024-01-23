1ec8c18010e9610f192d2efcfc688627f8a2de63
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-01-23 10:39:20 +0100

    [add] style

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,14 +1,17 @@
-<header class="container-fluid d-flex justify-content-between my-3">
 
-    <div class="logo-header">
-        <img  src="{{ asset('images/assets/Boolbnb-logo.png') }}" alt="">
+<header>
+    <div class="container d-flex justify-content-between">
+            <div class="logo">
+                <a href="http://localhost:5174/">
+                    <img src="{{ asset('images/assets/Boolbnb_logo.png') }}"  alt="logo">
+                </a>
+            </div>
+
+                    <div class="logout">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="log-out-button">Logout <i class="fa-solid fa-right-from-bracket"></i></button>
+                        </form>
+                    </div>
     </div>
-
-    <div class="logout">
-        <form id="logout-form" action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button class="log-out-button">Logout <i class="fa-solid fa-right-from-bracket"></i></button>
-        </form>
-    </div>
-
 </header>
--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <aside class="container-fluid">
-    <nav>
-        <ul class="d-flex">
+    <nav class="container">
+        {{-- <ul class="d-flex m-0 p-0">
             <li class="mb-3 mx-3"><a href="{{ route('admin.home') }}"><i class="fa-solid fa-chart-line"></i> Dashboard</a>
             </li>
             <li class="mb-3 mx-3"><a href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-list"></i> Lista
@@ -11,6 +11,18 @@
                     Messaggi</a></li>
             <li class="mb-3 mx-3"><a href="{{ route('admin.sponsors.index') }}"><i class="fa-solid fa-envelope"></i>
                     Sponsor</a></li>
-        </ul>
+        </ul> --}}
+
+        <div class="row row-cols-5 py-3">
+            <div class="col text-center"><a href="{{ route('admin.home') }}"><i class="fa-solid fa-chart-line"></i> Dashboard</a></div>
+            <div class="col text-center"><a href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-list"></i> Lista
+                Appartamenti</a></div>
+            <div class="col text-center"><a href="{{ route('admin.apartments.create') }}"><i class="fa-solid fa-folder-plus"></i>
+                Nuovo Appartamento</a></div>
+            <div class="col text-center"><a href="{{ route('admin.messages.index') }}"><i class="fa-solid fa-envelope"></i>
+                Messaggi</a></div>
+            <div class="col text-center"><a href="{{ route('admin.sponsors.index') }}"><i class="fa-solid fa-star"></i>
+                Sponsor</a></div>
+        </div>
     </nav>
 </aside>
