b5d04a9a77861023158090686d406a340885ddf6
titho93 <titho93.10@gmail.com>
2024-01-24 19:19:06 +0100

    [edit]apartment-list-plan_title

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,39 +1,42 @@
 @extends('layouts.admin')
 
 @section('content')
-
-        <h1>Lista appartamenti</h1>
-        <table class="table w-100">
-            <thead>
+    <h1>Lista appartamenti</h1>
+    <table class="table w-100">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Appartamento</th>
+                <th scope="col">Indirizzo</th>
+                <th scope="col">Sponsorizzazione</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($apartments as $apartment)
                 <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Appartamento</th>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($apartments as $apartment)
-                    <tr>
-                        <td>{{ $apartment->id }}</td>
-                        <td>{{ $apartment->title }}</td>
-                        <td>{{ $apartment->address }}</td>
-                        <td>
-                            <a class="btn btn-dark" href="{{ route('admin.apartments.show', $apartment) }}"><i
-                                    class="fa-solid fa-circle-info" style="color: #ffffff;"></i></a>
-                            <a class="btn btn-dark" href="{{ route('admin.apartments.edit', $apartment) }}"><i
-                                    class="fa-solid fa-pen-to-square"></i></a>
-                            <form class="d-inline-block" action="{{ route('admin.apartments.destroy', $apartment) }}"
-                                method="POST"
-                                onsubmit="return confirm ('Sei sicuro di voler eliminare questo appartamento dalla tua lista?')">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-dark"><i class="fa-solid fa-trash-can"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    <td>{{ $apartment->id }}</td>
+                    <td>{{ $apartment->title }}</td>
+                    <td>{{ $apartment->address }}</td>
+                    @foreach ($apartment->sponsors as $sponsor)
+                        {{ $sponsor->plan_title }}
+                    @endforeach
 
+                    <td>
+                        <a class="btn btn-dark" href="{{ route('admin.apartments.show', $apartment) }}"><i
+                                class="fa-solid fa-circle-info" style="color: #ffffff;"></i></a>
+                        <a class="btn btn-dark" href="{{ route('admin.apartments.edit', $apartment) }}"><i
+                                class="fa-solid fa-pen-to-square"></i></a>
+                        <form class="d-inline-block" action="{{ route('admin.apartments.destroy', $apartment) }}"
+                            method="POST"
+                            onsubmit="return confirm ('Sei sicuro di voler eliminare questo appartamento dalla tua lista?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-dark"><i class="fa-solid fa-trash-can"></i></button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
