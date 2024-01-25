35b295830ef80ff5f72105338e69dc369a201ee3
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-01-25 11:31:43 +0100

    [add] seeder csv funzionanti, da correggere il percorso per le immagini nello storage

--- database/seeders/ApartmentServiceSeeder.php
+++ database/seeders/ApartmentServiceSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Apartment;
 use App\Models\Service;
+use Illuminate\Support\Facades\DB;
 
 class ApartmentServiceSeeder extends Seeder
 {
@@ -15,9 +16,16 @@ class ApartmentServiceSeeder extends Seeder
     public function run(): void
     {
 
-        $apartment = Apartment::inRandomOrder()->first();
-        $service_id = Service::inRandomOrder()->first()->id;
+        // $apartment = Apartment::inRandomOrder()->first();
+        // $service_id = Service::inRandomOrder()->first()->id;
 
-        $apartment->services()->attach($service_id);
+        // $apartment->services()->attach($service_id);
+
+        $apartments = Apartment::all();
+        foreach($apartments as $apartment)
+            DB::table('apartment_service')->insert([
+                'apartment_id' => $apartment->id,
+                'service_id' => Service::inRandomOrder()->first()->id
+            ]);
     }
 }
