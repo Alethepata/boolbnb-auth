3877e8ef13d66e91fe39cb71b6edb3ef260295fa
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-01-30 11:50:04 +0100

    [add] payment style page

--- resources/views/dropin.blade.php
+++ resources/views/dropin.blade.php
@@ -5,19 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Payment</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
-    <p>Stai sponsorizzando <strong>{{$apartment->title}}</strong> con il piano <strong>{{$sponsor->plan_title}}</strong></p>
-    <div id="dropin-container"></div>
-    <button id="submit-button">Paga {{$sponsor->price}} &euro;</button>
+    <div class="container my-5">
+        <div class="row row-cols-2">
+            <div class="col p-5">
+                <div class="">Stai sponsorizzando <strong>{{$apartment->title}}</strong> con il piano <strong>{{$sponsor->plan_title}}</strong></div>
+                <div>Prezzo: <strong>{{$sponsor->price}} &euro;</strong></div>
+            </div>
+            <div class="col">
+                <div id="dropin-container" class=""></div>
+            </div>
+        </div>
+
+        <div class="row justify-content-end mt-3">
+            <div class="col-2">
+                <button id="submit-button" class="btn btn-primary w-100 ">Paga {{$sponsor->price}} &euro;</button>
+            </div>
+
+        </div>
+    </div>
+
+
 
     <script src="https://js.braintreegateway.com/web/dropin/1.30.0/js/dropin.min.js"></script>
     <script>
     document.addEventListener('DOMContentLoaded', function () {
         var sponsor = '{{ $sponsor }}'; // Recupera il valore di sponsor dalla variabile PHP
         var apartment = '{{ $apartment }}'; // Recupera il valore di apartment dalla variabile PHP
-
+        var sponsor_id = '{{$sponsor->id}}';
+        var apartment_id = '{{$apartment->id}}';
         var button = document.getElementById('submit-button');
         var csrfToken = document.querySelector('meta[name="csrf-token"]').content;
 
@@ -45,8 +65,8 @@
 
         var data = JSON.stringify({
             payment_method_nonce: nonce,
-            sponsor: sponsor,
-            apartment: apartment
+            sponsor: sponsor_id,
+            apartment: apartment_id
         });
 
         xhr.onreadystatechange = function () {
@@ -58,7 +78,7 @@
                     // Controlla se il pagamento è riuscito
                     if (response.success) {
                         // Esegui il redirect alla rotta /payment-success
-                        window.location.href = '/payment-success?sponsor=' + sponsor + '&apartment=' + apartment;
+                        window.location.href = '/payment-success?sponsor=' + sponsor_id + '&apartment=' + apartment_id;
                     }else{
                         window.location.href = '/payment-error';
                     }
