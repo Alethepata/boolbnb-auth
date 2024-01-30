fd240860185189322c94f5f41332c19f367c88d8
titho93 <titho93.10@gmail.com>
2024-01-30 11:02:16 +0100

    [add]listapartments-mobile

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>Lista appartamenti</h1>
+    {{-- Vista Desktop  --}}
     <table class="table w-100">
         <thead>
             <tr>
@@ -20,7 +21,7 @@
                         @if (count($apartment->sponsors) > 0)
                             @foreach ($apartment->sponsors as $sponsor)
                                 @if (!empty($sponsor->plan_title))
-                                <span class="badge rounded-pill text-bg-success">{{ $sponsor->plan_title }}</span>
+                                    <span class="badge rounded-pill text-bg-success">{{ $sponsor->plan_title }}</span>
                                 @endif
                             @endforeach
                         @else
@@ -46,4 +47,39 @@
             @endforeach
         </tbody>
     </table>
+    {{-- Fine Vista Desktop  --}}
+    {{-- Vista Mobile  --}}
+    @foreach ($apartments as $apartment)
+        <div class="card card-m" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{ $apartment->title }}</h5>
+                <p class="card-text">{{ $apartment->address }}</p>
+                <span>
+                    @if (count($apartment->sponsors) > 0)
+                        @foreach ($apartment->sponsors as $sponsor)
+                            @if (!empty($sponsor->plan_title))
+                                <span class="badge rounded-pill text-bg-success">{{ $sponsor->plan_title }}</span>
+                            @endif
+                        @endforeach
+                    @else
+                        <span class="badge rounded-pill text-bg-warning">Non sponsorizzato</span>
+                    @endif
+                </span>
+                <div class="action">
+                    <a class="btn" href="{{ route('admin.apartments.show', $apartment) }}"><i
+                            class="fa-solid fa-circle-info" style="color: #ffffff;"></i></a>
+                    <a class="btn" href="{{ route('admin.apartments.edit', $apartment) }}"><i
+                            class="fa-solid fa-pen-to-square"></i></a>
+                    <form class="d-inline-block" action="{{ route('admin.apartments.destroy', $apartment) }}"
+                        method="POST"
+                        onsubmit="return confirm ('Sei sicuro di voler eliminare questo appartamento dalla tua lista?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn"><i class="fa-solid fa-trash-can"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    {{-- Fine Vista Mobile  --}}
 @endsection
