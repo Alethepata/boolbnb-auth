51510ba246f413e62a7b84ab73b348be0779e0bc
Idedav <xdavide.correa@gmail.com>
2024-01-31 17:02:29 +0100

    [add] printServices function on show apartment

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="show-apartment">
-    <div class="row row-cols-2 align-items-center">
-        <div class="col">
-            <div class="image">
-                @if (substr($apartment->img, 0, 7) == 'uploads')
-                <img src="{{ asset('storage/' . $apartment->img) }}" alt="{{ $apartment->title }}">
-                @else
-                <img src="{{ asset($apartment->img) }}" alt="{{ $apartment->title }}">
-                @endif
+    <div class="show-apartment">
+        <div class="row row-cols-2 align-items-center">
+            <div class="col">
+                <div class="image">
+                    @if (substr($apartment->img, 0, 7) == 'uploads')
+                        <img src="{{ asset('storage/' . $apartment->img) }}" alt="{{ $apartment->title }}">
+                    @else
+                        <img src="{{ asset($apartment->img) }}" alt="{{ $apartment->title }}">
+                    @endif
+                </div>
             </div>
-        </div>
             <div class="col ps-5">
                 <div class="text-content">
                     <div class="text">
@@ -34,11 +34,7 @@
                     </div>
                     <div class="services-container">
                         <h5 class="servizi">Servizi aggiuntivi:</h5>
-                        @forelse ($apartment->services as $service)
-                            <span>{{ $service->title}}</span>
-                        @empty
-                            <span>Non ci sono servizi aggiuntivi</span>
-                        @endforelse
+                        <span id="service-list"></span>
                     </div>
                 </div>
                 <div class="graphic-container ">
@@ -56,145 +52,155 @@
         </div>
 
 
-    {{-- SCRIPT DEL GRAFICO --}}
+        {{-- SCRIPT DEL GRAFICO --}}
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <script>
-        const ctx = document.getElementById('myChart');
-        const ctx2 = document.getElementById('myChart2');
-        const views = @json($apartment->views);
-        const messages = @json($apartment->messages);
+        <script>
+            const services = @json($apartment->services);
+            const ctx = document.getElementById('myChart');
+            const ctx2 = document.getElementById('myChart2');
+            const views = @json($apartment->views);
+            const messages = @json($apartment->messages);
+            const servicesListHtml = document.getElementById('service-list');
 
-        console.log(views);
+            // funzione per i servizi
 
-        // Inizializzo 2 array di 12 zeri
-        let dataView = new Array(12).fill(0);
-        let dataMessages = new Array(12).fill(0);
-
-        // Ciclo  views
-        for (let view of views) {
-            // Ottengo la data della view
-            let date = new Date(view.date);
-
-            // Calcolo l'indice nell array di dati in base al mese e all'anno corrente
-            let now = new Date();
-            let months = (now.getFullYear() - date.getFullYear()) * 12 + now.getMonth() - date.getMonth();
-            let index = 11 - months;
-
-            // Incremento il conteggio per quel mese
-            if (index >= 0 && index < 12) {
-                dataView[index]++;
+            function printServices() {
+                serviceList = services.map(service => service.title);
+                return serviceList.join(" - ");
             }
-        }
 
-        // Ciclo messaggi
-        for (let message of messages) {
-            // Ottengo la data del messaggio
-            let date = new Date(message.created_at);
+            servicesListHtml.innerHTML = printServices();
 
-            // Calcolo l'indice nell array di dati in base al mese e all'anno corrente
-            let now = new Date();
-            let months = (now.getFullYear() - date.getFullYear()) * 12 + now.getMonth() - date.getMonth();
-            let index = 11 - months;
+            console.log(printServices());
+            // Inizializzo 2 array di 12 zeri
+            let dataView = new Array(12).fill(0);
+            let dataMessages = new Array(12).fill(0);
 
-            // Incremento il conteggio per quel mese
-            if (index >= 0 && index < 12) {
-                dataMessages[index]++;
+            // Ciclo  views
+            for (let view of views) {
+                // Ottengo la data della view
+                let date = new Date(view.date);
+
+                // Calcolo l'indice nell array di dati in base al mese e all'anno corrente
+                let now = new Date();
+                let months = (now.getFullYear() - date.getFullYear()) * 12 + now.getMonth() - date.getMonth();
+                let index = 11 - months;
+
+                // Incremento il conteggio per quel mese
+                if (index >= 0 && index < 12) {
+                    dataView[index]++;
+                }
             }
-        }
+
+            // Ciclo messaggi
+            for (let message of messages) {
+                // Ottengo la data del messaggio
+                let date = new Date(message.created_at);
+
+                // Calcolo l'indice nell array di dati in base al mese e all'anno corrente
+                let now = new Date();
+                let months = (now.getFullYear() - date.getFullYear()) * 12 + now.getMonth() - date.getMonth();
+                let index = 11 - months;
+
+                // Incremento il conteggio per quel mese
+                if (index >= 0 && index < 12) {
+                    dataMessages[index]++;
+                }
+            }
 
 
 
 
-        // FORMAT DATE
-        let currentDate;
-        let monthYear;
+            // FORMAT DATE
+            let currentDate;
+            let monthYear;
 
-        function formatDate(n) {
+            function formatDate(n) {
 
-            currentDate = new Date();
+                currentDate = new Date();
 
-            currentDate.setMonth(currentDate.getMonth() - n);
+                currentDate.setMonth(currentDate.getMonth() - n);
 
-            monthYear = currentDate.toLocaleString('it-IT', {
-                month: 'short',
-                year: 'numeric'
-            })
+                monthYear = currentDate.toLocaleString('it-IT', {
+                    month: 'short',
+                    year: 'numeric'
+                })
 
-            return monthYear;
-        }
+                return monthYear;
+            }
 
 
 
-        // GRAFICO VISUALIZZAZIONI
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: [formatDate(11),
-                    formatDate(10),
-                    formatDate(9),
-                    formatDate(8),
-                    formatDate(7),
-                    formatDate(6),
-                    formatDate(5),
-                    formatDate(4),
-                    formatDate(3),
-                    formatDate(2),
-                    formatDate(1),
-                    formatDate(0),
-                ],
-                datasets: [{
-                        label: 'Visualizzazioni',
-                        data: [
-                            dataView[0],
-                            dataView[1],
-                            dataView[2],
-                            dataView[3],
-                            dataView[4],
-                            dataView[5],
-                            dataView[6],
-                            dataView[7],
-                            dataView[8],
-                            dataView[9],
-                            dataView[10],
-                            dataView[11],
-                            dataView[12],
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Messaggi',
-                        data: [
-                            dataMessages[0],
-                            dataMessages[1],
-                            dataMessages[2],
-                            dataMessages[3],
-                            dataMessages[4],
-                            dataMessages[5],
-                            dataMessages[6],
-                            dataMessages[7],
-                            dataMessages[8],
-                            dataMessages[9],
-                            dataMessages[10],
-                            dataMessages[11],
-                            dataMessages[12],
-                        ],
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        ticks: {
-                            beginAtZero: true,
-                            stepSize: 1,
-                            precision: 0
+            // GRAFICO VISUALIZZAZIONI
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: [formatDate(11),
+                        formatDate(10),
+                        formatDate(9),
+                        formatDate(8),
+                        formatDate(7),
+                        formatDate(6),
+                        formatDate(5),
+                        formatDate(4),
+                        formatDate(3),
+                        formatDate(2),
+                        formatDate(1),
+                        formatDate(0),
+                    ],
+                    datasets: [{
+                            label: 'Visualizzazioni',
+                            data: [
+                                dataView[0],
+                                dataView[1],
+                                dataView[2],
+                                dataView[3],
+                                dataView[4],
+                                dataView[5],
+                                dataView[6],
+                                dataView[7],
+                                dataView[8],
+                                dataView[9],
+                                dataView[10],
+                                dataView[11],
+                                dataView[12],
+                            ],
+                            borderWidth: 1
+                        },
+                        {
+                            label: 'Messaggi',
+                            data: [
+                                dataMessages[0],
+                                dataMessages[1],
+                                dataMessages[2],
+                                dataMessages[3],
+                                dataMessages[4],
+                                dataMessages[5],
+                                dataMessages[6],
+                                dataMessages[7],
+                                dataMessages[8],
+                                dataMessages[9],
+                                dataMessages[10],
+                                dataMessages[11],
+                                dataMessages[12],
+                            ],
+                            borderWidth: 1
+                        }
+                    ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            ticks: {
+                                beginAtZero: true,
+                                stepSize: 1,
+                                precision: 0
+                            }
                         }
                     }
                 }
-            }
-        });
-    </script>
-@endsection
+            });
+        </script>
+    @endsection
